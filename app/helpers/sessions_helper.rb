92d1dd34df8bdd32c5d58162cfe2c5077cd63a69
Pawel Nuzka <pawelqus@Pawel-Nuzkas-MacBook-Pro.local>
2011-05-17 02:18:00 +0200

    Done with user edit/update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,15 @@
 module SessionsHelper
 
   def current_user=(user)
-     @current_user = user
+    @current_user = user
   end
   
   def current_user
-      @current_user ||= user_from_remember_token
+    @current_user ||= user_from_remember_token
+  end
+  
+  def current_user?(user)
+    @current_user == user
   end
   
   def sign_in(user)
@@ -21,7 +25,17 @@ module SessionsHelper
   def signed_in?
     !current_user.nil?
   end
+  
+  def deny_access
+    store_location
+    redirect_to signin_path, :notice => "Please sign in to access this page."
+  end
 
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    clear_return_to
+  end
+  
   private
 
     def user_from_remember_token
@@ -32,4 +46,11 @@ module SessionsHelper
       cookies.signed[:remember_token] || [nil, nil]
     end
     
+    def store_location
+      session[:return_to] = request.fullpath
+    end
+
+    def clear_return_to
+      session[:return_to] = nil
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -23,26 +23,26 @@ describe User do
     long_name_user.should_not be_valid
   end
   it "should accept valid email addresses" do
-      addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
-      addresses.each do |address|
-        valid_email_user = User.new(@attr.merge(:email => address))
-        valid_email_user.should be_valid
-      end
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@attr.merge(:email => address))
+      valid_email_user.should be_valid
     end
+  end
 
-    it "should reject invalid email addresses" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
-      addresses.each do |address|
-        invalid_email_user = User.new(@attr.merge(:email => address))
-        invalid_email_user.should_not be_valid
-      end
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@attr.merge(:email => address))
+      invalid_email_user.should_not be_valid
     end
-    it "should reject duplicate email addresses" do
-       # Put a user with given email address into the database.
-       User.create!(@attr)
-       user_with_duplicate_email = User.new(@attr)
-       user_with_duplicate_email.should_not be_valid
-   end
+  end
+  it "should reject duplicate email addresses" do
+     # Put a user with given email address into the database.
+     User.create!(@attr)
+     user_with_duplicate_email = User.new(@attr)
+     user_with_duplicate_email.should_not be_valid
+  end
    it "should reject email addresses identical up to case" do
        upcased_email = @attr[:email].upcase
        User.create!(@attr.merge(:email => upcased_email))
@@ -97,20 +97,39 @@ describe User do
      
      describe "authenticate method" do
 
-           it "should return nil on email/password mismatch" do
-             wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
-             wrong_password_user.should be_nil
-           end
+       it "should return nil on email/password mismatch" do
+         wrong_password_user = User.authenticate(@attr[:email], "wrongpass")
+         wrong_password_user.should be_nil
+       end
 
-           it "should return nil for an email address with no user" do
-             nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
-             nonexistent_user.should be_nil
-           end
+       it "should return nil for an email address with no user" do
+         nonexistent_user = User.authenticate("bar@foo.com", @attr[:password])
+         nonexistent_user.should be_nil
+       end
 
-           it "should return the user on email/password match" do
-             matching_user = User.authenticate(@attr[:email], @attr[:password])
-             matching_user.should == @user
-           end
-         end
+       it "should return the user on email/password match" do
+         matching_user = User.authenticate(@attr[:email], @attr[:password])
+         matching_user.should == @user
+       end
+     end
    end
+   describe "admin attribute" do
+
+      before(:each) do
+        @user = User.create!(@attr)
+      end
+
+      it "should respond to admin" do
+        @user.should respond_to(:admin)
+      end
+
+      it "should not be an admin by default" do
+        @user.should_not be_admin
+      end
+
+      it "should be convertible to an admin" do
+        @user.toggle!(:admin)
+        @user.should be_admin
+      end
+    end
 end
